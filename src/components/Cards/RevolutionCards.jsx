1f60968813b6a88391245d90e47c1ee3468ca495
Henry Okeke <iamhenryokeke@gmail.com>
2023-05-23 21:46:02 +0100

    created card for revolution section

---
+++ src/components/Cards/RevolutionCards.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+import {ReactComponent as EthereumIcon} from "../../assets/svg/Ethereum-Logo Large.svg";
+
+const RevolutionCards = ({heading, description}) => {
+    
+  return (
+    <div className="flex flex-col gap-6 items-center text-center">
+      <EthereumIcon/>
+      <h1 className="text-white font-semibold text-2xl lg:text-[28px]">{heading}</h1>
+      <p className="text-[#ffffff80] text-[14px] lg:text-[20px]">{description}</p>
+    </div>
+  )
+}
+
+RevolutionCards.propTypes = {
+    heading: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired
+}
+
+export default RevolutionCards;
+
+
+
