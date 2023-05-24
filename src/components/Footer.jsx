0938d2977080aa745e3bc4c41ca10523316a24ba
Henry Okeke <iamhenryokeke@gmail.com>
2023-05-24 21:30:22 +0100

    implemented design Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ReactComponent as TwitterIcon } from '../assets/svg/Twitter .svg'
+
+const Footer = () => {
+  return (
+    <div className='flex justify-between items-center pb-14 pt-20 lg:pb-24 lg:pt-28'>
+      <h1 className="font-bold text-2xl">
+            <span className="text-white">de</span><span className="text-indigo-1000">spark</span>
+      </h1>
+      <TwitterIcon/>
+    </div>
+  )
+}
+
+export default Footer
