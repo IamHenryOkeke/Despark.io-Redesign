2fd87ef146a57d67ebd355a09ca503e13f1c40d3
Henry Okeke <iamhenryokeke@gmail.com>
2023-05-25 22:34:11 +0100

    removed padding on x axis

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
 import { ReactComponent as TwitterIcon } from '../assets/svg/Twitter .svg'
 
 const Footer = () => {
   return (
-    <div className='flex justify-between items-center pb-14 pt-20 lg:pb-24 lg:pt-28'>
+    <div className='flex justify-between items-center pb-14 lg:pb-24 lg:pt-28'>
       <h1 className="font-bold text-2xl">
             <span className="text-white">de</span><span className="text-indigo-1000">spark</span>
       </h1>
@@ -13,3 +12,4 @@ const Footer = () => {
 }
 
 export default Footer
+
