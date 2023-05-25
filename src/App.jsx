be4eb7f8eac61a3dfaa0c7aeb416ddfca47cd83a
Henry Okeke <iamhenryokeke@gmail.com>
2023-05-25 22:32:46 +0100

    imported components into App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import Navbar from "./components/Navbar.jsx"
+import Contact from "./components/Contact.jsx"
+import Feedback from "./components/Feedback.jsx"
+import Footer from "./components/Footer.jsx"
+import Future from "./components/Future.jsx"
+import Hero from "./components/Hero.jsx"
+import Revolution from "./components/Revolution.jsx"
+
 
 const App = () => {
 
   return (
-    <>
-      <h1 className="text-6xl text-red-500">Hello project</h1>
-    </>
+    <div className="px-4 lg:px-32 font-Body bg-black">
+      <Navbar/>
+      <Hero/>
+      <Revolution/>
+      <Future/>
+      <Feedback/>
+      <Contact/>
+      <Footer/> 
+    </div>
   )
 }
 
